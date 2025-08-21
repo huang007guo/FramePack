1584f0b5d513277a8af90ea000d7b8af6f081a3d
huangguoliang <huangguoliang@renwohua.com>
2025-08-21 21:51:31 +0800

    refactor(webui): 优化 run-my.bat 脚本
    - 添加管理员权限获取功能
    - 设置和删除环境变量
    - 改进字符编码支持
    - 优化目录切换逻辑
    - 添加参数传递功能

--- bat/run-my.bat
+++ bat/run-my.bat
@@ -1,5 +1,24 @@
 @echo off
-cd %~dp0webui
+:: 设置到全局环境变量
+setx FRAMEPACK_ARGS "%~1"
+:: 获得管理员权限
+Net session >nul 2>&1 || mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0","","runas",1)(window.close)&&exit
+:: 进入conda环境
+:: CALL conda activate wan2.1
+
+@REM 支持中文
+chcp 65001
+set PYTHONIOENCODING=utf-8
+@REM 移动到项目根目录,更目录在当前目录下
+cd /d %~dp0webui
+@REM 输出当前目录
+echo 当前运行目录: %CD%
+
+SET ARGS=%FRAMEPACK_ARGS%
+:: 删除环境变量
+setx FRAMEPACK_ARGS ""
+echo ARGS: %ARGS%
+
 REM call environment.bat
 
 set DIR=%~dp0system
@@ -14,5 +33,5 @@ set PIP_INSTALLER_LOCATION=%DIR%\python\get-pip.py
 
 "%DIR%\python\python.exe" %~dp0webui\run-sync.py --max_run_time="7,1,1" --del_source_file --only_remain_last_file --shutdown --gpu_memory_preservation=12.0 --resolution=1024 --total_second_length=10.0 -S=E:\tmp\image-to-video\run --fps=20 --prompt="girl"
 
-:done
+REM :done
 pause
